691b8a50310b334df0f10169acfa342686efb5b8
dboulytchev <dboulytchev@gmail.com>
2013-04-01 18:59:39 +0000

    Fixed bug in non-extensible transformers generation

--- regression/test002.ml
+++ regression/test002.ml
@@ -7,14 +7,14 @@ module Expr =
       | `Const of [int] 
       | `Binop of [int -> int -> int] * [string] * 'self t * 'self t
     ]
-
+(*
     class virtual ['me, 'a, 'b] t_t =
       object (this)
         method virtual m_Var   : 'a -> 'me -> string -> 'b
         method virtual m_Const : 'a -> 'me -> int -> 'b
         method virtual m_Binop : 'a -> 'me -> (int -> int -> int) -> string -> ('a, 'me, 'b) Generic.a -> ('a, 'me, 'b) Generic.a -> 'b
       end
-
+*)
     class ['a] toString =
       object (this)
         inherit ['a, unit, string] t_t
@@ -50,7 +50,7 @@ module Stmt =
   end
 let _ =
   let e = `Binop ((+), "+", `Const 1, `Var "a") in
-  let s = Expr.t.Generic.gcata (*Generic.apply*) (new Expr.toString) () e in
-  let v = Expr.t.Generic.gcata (*Generic.apply*) (new Expr.eval (fun "a" -> 2)) () e in
+  let s = Expr.t.Generic.gcata (new Expr.toString) () e in
+  let v = Expr.t.Generic.gcata (new Expr.eval (fun "a" -> 2)) () e in
   Printf.printf "%s\n" s;
   Printf.printf "%d\n" v
