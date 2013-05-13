e781ae7cb1c8f69f4981b3ade7d594e9615eac34
dboulytchev <dboulytchev@gmail.com>
2013-05-13 16:23:01 +0000

    Reversed processing/protected attribution

--- regression/test001.ml
+++ regression/test001.ml
@@ -3,14 +3,14 @@ open Generic
 generic 'l t = 
     R 
   | W 
-  | L  of [string] 
-  | S  of [string] 
-  | B  of [int -> int -> int] * [string]
+  | L  of string 
+  | S  of string 
+  | B  of (int -> int -> int) * string
   | E
-  | C  of [int]
-  | J  of 'l
-  | JT of 'l
-  | JF of 'l
+  | C  of int
+  | J  of ['l]
+  | JT of ['l]
+  | JF of ['l]
 
 class toString =
   object (this)
