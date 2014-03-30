c10b38abd63605ae735bcacb2d0882ce671cb56e
dboulytchev@gmail.com <dboulytchev@gmail.com@4ccc5c40-2f02-293e-5767-744915a0d2f8>
2014-03-30 22:16:23 +0000

    23/24 workarounds found; not to fix

--- trunk/regression/test024.ml
+++ trunk/regression/test024.ml
@@ -1,17 +1,17 @@
 @type 'a a = [`A of 'a | `B of GT.string] deriving show, eq, compare
 @type 'a b = [`C of 'a | `D of GT.string] deriving show, eq, compare
-@type 'a c = ['a a | 'a b] deriving show, eq, compare
+@type ('a, 'b) c = ['a a | 'b b] deriving show, eq, compare
 
 let _ =
   let x = `A 3 in
   let y = `C 2 in
   Printf.printf "%s\n" (GT.transform(a) (fun _ x -> string_of_int x) (new @show[a]) () x);
   Printf.printf "%s\n" (GT.transform(b) (fun _ x -> string_of_int x) (new @show[b]) () y);
-  Printf.printf "%s\n" (GT.transform(c) (fun _ x -> string_of_int x) (new @show[c]) () x);
-  Printf.printf "%s\n" (GT.transform(c) (fun _ x -> string_of_int x) (new @show[c]) () y);
+  Printf.printf "%s\n" (GT.transform(c) (fun _ x -> string_of_int x) (fun _ x -> string_of_int x) (new @show[c]) () x);
+  Printf.printf "%s\n" (GT.transform(c) (fun _ x -> string_of_int x) (fun _ x -> string_of_int x) (new @show[c]) () y);
   Printf.printf "%b\n" (GT.transform(a) (rewrap_a (fun x y -> x = y)) (new @eq[a]) (`t x) x);
   Printf.printf "%b\n" (GT.transform(b) (rewrap_b (fun x y -> x = y)) (new @eq[b]) (`t y) y);
-  Printf.printf "%b\n" (GT.transform(c) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t x) x);
-  Printf.printf "%b\n" (GT.transform(c) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t y) y);
-  Printf.printf "%b\n" (GT.transform(c) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t x) y)
+  Printf.printf "%b\n" (GT.transform(c) (rewrap_a (fun x y -> x = y)) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t x) x);
+  Printf.printf "%b\n" (GT.transform(c) (rewrap_a (fun x y -> x = y)) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t y) y);
+  Printf.printf "%b\n" (GT.transform(c) (rewrap_a (fun x y -> x = y)) (rewrap_b (fun x y -> x = y)) (new @eq[c]) (`t x) y)
 
