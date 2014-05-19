6e16eb03dd1ec2abb5fde281e09bc4637ff0c18b
dboulytchev@gmail.com <dboulytchev@gmail.com@4ccc5c40-2f02-293e-5767-744915a0d2f8>
2014-05-19 19:17:06 +0000

    Fixed test031

---
+++ trunk/regression/test031.ml
@@ -0,0 +1,35 @@
+open GT
+
+@type ('a, 'b) t = {x: int; y: string; a: 'a; b: 'b} with show, map
+
+class ['a, 'b] print =
+  object 
+    inherit ['a, unit, 'b, unit, unit, unit] @t
+    method value _ _ x y a b = 
+      Printf.printf "%d\n" x;
+      Printf.printf "%s\n" y; 
+      a.fx (); 
+      b.fx ()
+  end
+
+let _ =
+  Printf.printf "%s\n" 
+    (transform(t) 
+       (fun _ a -> string_of_int a) 
+       (fun _ x -> x)
+       (new @show[t])
+       ()
+       (transform(t)
+	  (fun _ a -> int_of_string a)
+	  (fun _ `B -> "`B")
+	  (new @map[t])
+	  ()
+	  {x=1; y="2"; a="3"; b=`B}
+       )
+    );
+  transform(t) 
+    (fun _ a -> Printf.printf "%s\n" a) 
+    (fun _ -> function `B -> Printf.printf "`B\n") 
+    (new print) 
+    () 
+    {x=1; y="2"; a="a"; b=`B}
