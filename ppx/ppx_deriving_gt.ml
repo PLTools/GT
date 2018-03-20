848718ec1ce242c2622f1f396aba28d4d28fa2ab
kakadu <kakadu.hafanana@gmail.com>
2018-03-20 20:19:33 +0300

    WIP

--- ppx/ppx_deriving_gt.ml
+++ ppx/ppx_deriving_gt.ml
@@ -11,12 +11,28 @@ open Ppx_core
 
 module Type_conv = Ppx_type_conv.Std.Type_conv
 
+module Attrs = struct
+  let ignore =
+    Attribute.declare "hash.ignore"
+      Attribute.Context.label_declaration
+      Ast_pattern.(pstr nil)
+      ()
+
+  (* let no_hashing =
+   *   Attribute.declare "hash.no_hashing"
+   *     Attribute.Context.label_declaration
+   *     Ast_pattern.(pstr nil)
+   *     ()
+   * ;; *)
+end
+
 module Sexp_of = struct
   module E = Ppx_gt_expander
   let name = "gt"
 
   let str_type_decl : (_, _) Type_conv.Generator.t =
     Type_conv.Generator.make
+      ~attributes:[ Attribute.T Attrs.ignore ]
       Type_conv.Args.(empty +> flag "show" +> flag "gmap" +> flag "foldl")
       E.str_type_decl_implicit
 
