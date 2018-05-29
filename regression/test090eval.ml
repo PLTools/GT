b40bf6e91ef92e49bdb02ca982dab7bc457fd313
kakadu <kakadu.hafanana@gmail.com>
2018-05-29 21:28:01 +0300

    Make test090 compilable

--- regression/test090eval.ml
+++ regression/test090eval.ml
@@ -31,7 +31,7 @@ end
 module LetRec = struct
   @type ('name, 'term) t = [`LetRec of 'name * 'term * 'term] with show,eval;;
 
-  class ['me, 'mew2, 'extra] de_bruijn ft = object
+  class ['me, 'me2, 'extra] de_bruijn ft = object
     inherit [string, unit, 'me, 'me2, 'env, 'extra] eval_t_t
         (fun _ -> assert false)
         (fun _ _ -> ())
@@ -47,30 +47,34 @@ end
 
 @type ('n, 'b) t =
    [ ('n, ('n, 'b) t) Lam.t
-   (* | ('b, ('n, 'b) t) Abs.t
-    * | ('b, ('n, 'b) t) Let.t *)
+   | ('b, ('n, 'b) t) Abs.t
+   | ('b, ('n, 'b) t) Let.t
    | ('b, ('n, 'b) t) LetRec.t
    ] with show,eval
 ;;
 @type named    = (GT.string, GT.string) t with show;;
 @type nameless = (GT.int, GT.unit) t with show;;
 
-class de_bruijn fself = object
-  inherit [string, int, string, nameless, string list, 'extra] eval_t_t
-      fself (fun _ -> assert false) fself
+class ['extra] de_bruijn fself = object
+  inherit [string, int, string, unit, 'env, 'extra] eval_t_t
+      fself (fun _ _ -> 100500) (fun _ _ -> ())
+  constraint 'env = string list
   inherit [named, nameless, 'extra]    Abs.de_bruijn fself
   inherit [named, nameless, 'extra]    Let.de_bruijn fself
   inherit [named, nameless, 'extra] LetRec.de_bruijn fself
 end
 
 let convert term =
-  fix0 (fun f -> transform(t) (new de_bruijn f)) [] term
+  GT.fix0 (fun f -> GT.transform(t) (new de_bruijn f)) [] term
 
 let _ =
   let l = `App (`Abs ("x", `Var "x"), `Abs ("y", `Var "y")) in
-  Printf.printf "Original: %s\n" (show(named) l);
+  Printf.printf "Original: %s\n" (GT.show(named) l);
   let m = `App (`Abs ((), `Var 1), `Abs ((), `Var 2)) in
-  Printf.printf "Nameless : %s\n" (show(nameless) m);
-  Printf.printf "Converted: %s\n" (show(nameless) @@ convert l);
-  Printf.printf "Converted: %s\n" (show(nameless) @@ convert (`Let    ("z", `Abs ("x", `Var "x"), `Abs ("x", `Abs ("y", `App (`Var "x", `Var "z"))))));
-  Printf.printf "Converted: %s\n" (show(nameless) @@ convert (`LetRec ("z", `App (`Abs ("x", `Var "x"), `Var "z"), `Abs ("x", `Abs ("y", `App (`Var "x", `Var "z"))))))
+  Printf.printf "Nameless : %s\n" (GT.show(nameless) m);
+  Printf.printf "Converted: %s\n" (GT.show(nameless) @@ convert l);
+  Printf.printf "Converted: %s\n" @@ GT.show(nameless) @@ convert @@
+  `Let    ("z", `Abs ("x", `Var "x"), `Abs ("x", `Abs ("y", `App (`Var "x", `Var "z"))));
+  Printf.printf "Converted: %s\n" @@ GT.show(nameless) @@ convert @@
+  `LetRec ("z", `App (`Abs ("x", `Var "x"), `Var "z"),
+           `Abs ("x", `Abs ("y", `App (`Var "x", `Var "z"))))
