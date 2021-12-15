cf75291863fa70a89389109c0cf7f5f5a8fc3987
kakadu <kakadu@pm.me>
2021-12-15 18:01:23 +0300

    Ally ocamlformat in common/

--- common/expander.mli
+++ common/expander.mli
@@ -1,6 +1,6 @@
 (*
  * Generic Transformers PPX syntax extension.
- * Copyright (C) 2016-2019
+ * Copyright (C) 2016-2021
  *   Dmitrii Kosarev aka Kakadu
  * St.Petersburg State University, JetBrains Research
  *)
@@ -10,34 +10,36 @@
 open Ppxlib
 
 (** Arguments of plugin are empty *)
-type config_plugin = Skip | Use of (longident * expression) list
+type config_plugin =
+  | Skip
+  | Use of (longident * expression) list
 
-module Make : functor (Helpers: GTHELPERS_sig.S) -> sig
+module Make : functor (Helpers : GTHELPERS_sig.S) -> sig
+  open Helpers
 
-open Helpers
+  val str_type_decl_many_plugins
+    :  loc:loc
+    -> Str.t list
+    -> (Base.string * config_plugin) list
+    -> Ppxlib.rec_flag * Ppxlib.type_declaration list
+    -> Str.t HelpersBase.List.t
 
-val str_type_decl_many_plugins: loc:loc ->
-  Str.t list ->
-  (Base.string * config_plugin) list ->
-  Ppxlib.rec_flag * Ppxlib.type_declaration list ->
-  Str.t HelpersBase.List.t
-
-val sig_type_decl_many_plugins: loc:loc ->
-  Sig.t list ->
-  (Base.string * config_plugin) list ->
-  Ppxlib.rec_flag * Ppxlib.type_declaration list ->
-  Sig.t HelpersBase.List.t
-(*
+  val sig_type_decl_many_plugins
+    :  loc:loc
+    -> Sig.t list
+    -> (Base.string * config_plugin) list
+    -> Ppxlib.rec_flag * Ppxlib.type_declaration list
+    -> Sig.t HelpersBase.List.t
+  (*
 val str_type_ext_many_plugins: loc:loc ->
   Str.t list ->
   (Base.string * config_plugin) list ->
   Ppxlib.type_extension ->
   Str.t HelpersBase.List.t
 *)
-
 end
 
 (** Registers a plugin. See {! Plugin_intf.PluginRes } for plugin interface. *)
-val register_plugin: string -> (module Plugin_intf.MAKE) -> unit
+val register_plugin : string -> (module Plugin_intf.MAKE) -> unit
 
-val get_registered_plugins: unit -> string list
+val get_registered_plugins : unit -> string list
